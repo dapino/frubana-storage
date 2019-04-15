c9024226047d924ad8be96259b095fbca6530d2d
Daniel Pino <dapino@outlook.com>
2019-04-15 00:51:42 -0500

    Create example data json

---
+++ src/services/json-generator-base.js
@@ -0,0 +1,15 @@
+/*
+Example used to generate data-example with https://next.json-generator.com
+[
+    {
+        'repeat(1, 100)': {
+            id: '{{integer(1, 100)}}',
+            category: '{{random("perecederos", "fragiles", "pesados")}}',
+            "size": '{{integer(1, 2)}}',
+            "product": {
+                "name": '{{random("banana", "apple", "strawberry")}}',
+            }
+        }
+    }
+]
+*/
