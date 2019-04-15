90a011a8a798daf653089a2c70ddab2ea5c1ef5a
Daniel Pino <dapino@outlook.com>
2019-04-15 00:50:15 -0500

    Components structure

---
+++ src/components/atoms/Product-box.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ProductBox extends Component {
+    render() {
+        return (
+            <div className="Product-box">
+
+            </div>
+        )
+    }
+}
+
+export default ProductBox;
---
+++ src/components/templates/Default-template.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class DefaultTemplate extends Component {
+    render() {
+        return <h1>template</h1>
+    }
+}
+
+export default DefaultTemplate;
